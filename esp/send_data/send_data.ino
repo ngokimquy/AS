bafb7e59912801b3f9f95fcfd69010f6df09cc04
ngokimquy <31992923+ngokimquy@users.noreply.github.com>
2018-07-29 00:29:19 +0700

    ok

--- esp/send_data/send_data.ino
+++ esp/send_data/send_data.ino
@@ -6,7 +6,7 @@ const char* ssid = "Tang 1";          //Tên mạng Wifi mà Socket server của
 const char* password = "sese0407";  //Pass mạng wifi ahihi, anh em rãnh thì share pass cho mình với.
  
 char host[] = "34.230.77.115";  //Địa chỉ IP dịch vụ, hãy thay đổi nó theo địa chỉ IP Socket server của bạn.
-int port = 8080;                  //Cổng dịch vụ socket server do chúng ta tạo!
+int port = 3000;                  //Cổng dịch vụ socket server do chúng ta tạo!
  
 //từ khóa extern: dùng để #include các biến toàn cục ở một số thư viện khác. Trong thư viện SocketIOClient có hai biến toàn cục
 // mà chúng ta cần quan tâm đó là
