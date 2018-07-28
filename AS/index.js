ae320611acd3d15054c5d1ee3613781f15b60562
ngokimquy <31992923+ngokimquy@users.noreply.github.com>
2018-07-28 17:22:30 +0000

    port

--- AS/index.js
+++ AS/index.js
@@ -6,7 +6,7 @@ app.set("views", "./views");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(8016);
+server.listen(3000);
 
 io.on("connection", function(socket){
 
